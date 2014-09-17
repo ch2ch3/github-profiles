d45839b74d57b6f50724afb15a96f90c834a0642
Ethel Ng <ethel.ng@gmail.com>
2014-09-17 18:35:24 +0100

    Added alert if user could not be found

--- public/js/lib/GithubProfiles.js
+++ public/js/lib/GithubProfiles.js
@@ -5,6 +5,8 @@ $('document').ready(function(){
 
 		var userData = $.get(url, function(user){
 			$('<article class="profile"><section class="avatar"><img src="' + user.avatar_url + '"></section><section class="statistics"><h3><a href="' + user.html_url + '">' + user.login + '</a></h3><strong>Repos: ' + user.public_repos + '</strong><strong>Followers: ' + user.followers + '</strong></section><br clear="all"></article>').prependTo('.profile-container');
+		}).fail(function(){
+			alert('Could not find this user.');
 		});
 	});
 });
