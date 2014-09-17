c85d5f2373d2746507057f8096390b0dfe78abf1
Ethel Ng <ethel.ng@gmail.com>
2014-09-17 18:10:39 +0100

    Getting user data and displaying it

--- public/js/lib/GithubProfiles.js
+++ public/js/lib/GithubProfiles.js
@@ -1,6 +1,10 @@
 $('document').ready(function(){
 	$('#add-profile').on('submit', function(event){
 		event.preventDefault();
-		alert("test");
-	})
+		var url = "https://api.github.com/users/" + $('#username').val();
+
+		var userData = $.get(url, function(user){
+			$('<article class="profile"><section class="avatar"><img src="' + user.avatar_url + '"></section><section class="statistics"><h3><a href="' + user.html_url + '">' + user.login + '</a></h3><strong>Repos: ' + user.public_repos + '</strong><strong>Followers: ' + user.followers + '</strong></section><br clear="all"></article>').prependTo('.profile-container');
+		});
+	});
 });
